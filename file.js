726bc6c1ec8af34fe1aaf8362024e0f8cee1ef03
b-paramesh <bhupathipramesh2025@gmail.com>
2025-07-30 11:49:07 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+document.write("hello");
+alert("paramesh");
+let a=10;
+let b=94875;
+console.log("erhfqwdsx");
+document.getElementsByClassName("heading").textContent="FSD";
